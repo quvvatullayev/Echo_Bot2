6e36f60a8487756882974e655c86ae3db7f1fc1f
tajriba <quvvatullayev@gmail.com>
2022-08-13 21:38:23 +0300

    fexsd

--- bot.py
+++ bot.py
@@ -1,16 +1,29 @@
-import requests, json
+import json, requests
 
-def Bot_info(data:json) -> json:
-    data = data['result']
-    for i in data:
-        userid = i['message']['from'].get('id')
-        usertext = i['message'].get('text')
-    url = 'https://api.telegram.org/bot5567524975:AAHH4ioN3ZGUXbzPPPrXNk2tdWJU3O_fFyk/sendMessage'
-    r = requests.get(url, params={'chat_id':userid, 'text':usertext})
+TOKIN = '5567524975:AAHH4ioN3ZGUXbzPPPrXNk2tdWJU3O_fFyk'
 
-"""send a request to the function"""
-            #URL request
-url = 'https://api.telegram.org/bot5567524975:AAHH4ioN3ZGUXbzPPPrXNk2tdWJU3O_fFyk/getUpdates'
-r = requests.get(url)
-h = r.json()
-x = Bot_info(h)
+def getUpdet():
+    url = f'https://api.telegram.org/bot{TOKIN}/getUpdates'
+    data = requests.get(url)
+    return data.json()
+
+def sendMessage(userId, userText):
+    url = f'https://api.telegram.org/bot{TOKIN}/sendMessage'
+    data = requests.get(url, params={'chat_id':userId, 'text':userText})
+    return data.json()
+
+def dataBeth(data):
+    update_id = data[-1]['update_id']
+    userText = data[-1]['message'].get('text')
+    userId = data[-1]['message']['chat']['id']
+    return update_id, userId, userText
+
+s = ''
+while True:
+    result = getUpdet()
+    dB = dataBeth(result['result'])
+    update_id,userId,userText = dB
+
+    if s != update_id:
+        sendMessage(userId, userText)
+        s = update_id
